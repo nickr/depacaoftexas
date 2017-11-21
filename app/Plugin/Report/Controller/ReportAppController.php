437b0d8fbcbd7a681f3977c6809b4a45bc1fac01
Nick Robillard <nick@ewdd.com>
2017-11-20 21:49:10 -0600

    delete cakephp 3

--- app/Plugin/Report/Controller/ReportAppController.php
+++
@@ -1,8 +0,0 @@
-<?php
-App::uses('AppController', 'Controller');
-//App::uses('PortalAppController', 'Portal.Controller');
-class ReportAppController extends AppController { 
-
-}
- 
- 
--- app/Plugin/Report/webroot/js/_pd_boostrap-carousel-addons.js
+++
@@ -1,9 +0,0 @@
- $(window).load(function(){
- $(document).ready(function() {
-    $('.carousel').each(function(){
-        $(this).carousel({
-            interval: 15000
-        });
-    });
-});​ 
- }
