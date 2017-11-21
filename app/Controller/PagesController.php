bf3d3c976a23004006dcea32b64773a070ebd607
Nick Robillard <nick@ewdd.com>
2017-11-20 21:49:28 -0600

    delete cakephp 3

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -4,18 +4,18 @@
  *
  * This file will render views from views/pages/
  *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
  * @package       app.Controller
  * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
 App::uses('AppController', 'Controller');
@@ -26,7 +26,7 @@ App::uses('AppController', 'Controller');
  * Override this controller by placing a copy in controllers directory of an application
  *
  * @package       app.Controller
- * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
+ * @link https://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
 class PagesController extends AppController {
 
@@ -35,42 +35,69 @@ class PagesController extends AppController {
  *
  * @var array
  */
-	public $uses = array();
-
+	public $uses = array(
+		'Page',
+        'PageDetail'
+	);
+	
 /**
  * Displays a view
  *
- * @return void
+ * @return CakeResponse|null
+ * @throws ForbiddenException When a directory traversal attempt.
  * @throws NotFoundException When the view file could not be found
- *	or MissingViewException in debug mode.
+ *   or MissingViewException in debug mode.
  */
-	public function display() {
-		$path = func_get_args();
+ 	
+    public function pluginSetup() {
+        
+        //These Two Lines are Required
+        parent::pluginSetup();
+    }
+    
+    
+    function beforeFilter(){
+        parent::beforeFilter();
+        
+        
+    }
 
-		$count = count($path);
-		if (!$count) {
-			return $this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
-
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-
-		try {
-			$this->render(implode('/', $path));
-		} catch (MissingViewException $e) {
-			if (Configure::read('debug')) {
-				throw $e;
-			}
-			throw new NotFoundException();
-		}
+    public $components = array( 'RequestHandler', 'Paginator', 'Session');
+    
+ 	public function home() {
+ 		$recs = $this->PageDetail->find('all', array(
+            'conditions' => array(
+                'PageDetail.page_id IS NULL'
+            ),
+        ));
+        
+        $this->set('recs', $recs);
+        $this->set('title_for_layout', 'Home' );
+	}
+	
+	public function display($id = null) {
+		$recs = $this->Page->find('first', array(
+            'conditions' => array(
+                'Page.id' => $id
+            ),
+            'contain'=>array(
+            	'PageDetail'=>array(
+            	)
+            ),
+        ));
+        
+        $this->set('recs', $recs);
+        
+        $this->set('breadcrumbs', array(
+            array('title'=>$recs['Page']['name'], 'link'=>array('controller'=>$recs['Page']['controller'], 'action'=>$recs['Page']['action'], $recs['Page']['id'])),
+        ));
+        
+        $this->set('title_for_layout', $recs['Page']['name'] );
+	}
+	
+	public function getMenu() {
+		$menu = $this->Page->getmenu();
+		
+		return $menu;
 	}
 }
